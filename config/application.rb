92b11c9cd24344ef3a9d31c926b82dfc49845d71
muratayusuke <info@muratayusuke.com>
2015-04-30 12:00:03 +0900

    update application.rb

--- config/application.rb
+++ config/application.rb
@@ -1,14 +1,14 @@
 require File.expand_path('../boot', __FILE__)
 
 # Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_view/railtie'
+require 'sprockets/railtie'
+# require 'rails/test_unit/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -37,5 +37,9 @@ module Gupppy
     config.assets.paths << `bundle show bitters`.strip + '/app/assets/stylesheets/'
 
     config.logger = Logger.new($stdout)
+
+    config.generators do |g|
+      g.helpers false
+    end
   end
 end
